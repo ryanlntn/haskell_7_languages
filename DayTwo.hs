5e771f776db6a550593d1ed5012a4fafb7798aeb
Ryan Linton <ryanl@clearsightstudio.com>
2015-12-09 22:14:25 +0900

    Write a sort that takes a list and returns a sorted list.

--- DayTwo.hs
+++ DayTwo.hs
@@ -1,1 +1,8 @@
 module DayTwo where
+    sorted :: Ord a => [a] -> [a]
+    sorted []    = []
+    sorted (h:t) = sorted lesser ++ [h] ++ sorted greater
+      where
+        lesser  = filter (< h) t
+        greater = filter (>= h) t
+
