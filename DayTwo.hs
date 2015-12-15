588abfdbac598f8079f2320aca7857e8a32e9336
Ryan Linton <ryanl@clearsightstudio.com>
2015-12-15 22:26:36 +0900

    Write a Haskell function to convert a string to a number.

--- DayTwo.hs
+++ DayTwo.hs
@@ -1,4 +1,6 @@
 module DayTwo where
+    import Data.List
+
     sorted :: Ord a => [a] -> [a]
     sorted []    = []
     sorted (h:t) = sorted lesser ++ [h] ++ sorted greater
@@ -6,3 +8,8 @@ module DayTwo where
         lesser  = filter (< h) t
         greater = filter (>= h) t
 
+    sortedBy :: (Ord a) => (a -> a -> Ordering) -> [a] -> [a]
+    sortedBy comparator list = sortBy comparator list
+
+    convertStringToNumber s = read (filter p s) :: Float
+      where p x = x /= '$' && x /= ','
